865ce3d10f6f9abe1ce256928f2f3a4f4dbe50c8
varshitha0699 <43058154+varshitha0699@users.noreply.github.com>
2019-09-11 23:39:06 +0530

    Update lab3_b.py

--- lab3_b.py
+++ lab3_b.py
@@ -1,12 +1,18 @@
 import random
 import string
-def pwd(len):
-    res=" "
-    str=string.ascii_letters+string.digits+string.punctuation
-    for i in range(len):
-       res=res+random.choice(str)
-    return res
-n=int(input("enter the length of a string"))
-print(pwd(n))
-   
 
+
+def ch(len):
+    res=" "
+    str1 = string.ascii_letters + string.digits + string.punctuation
+    res=res+random.choice(string.ascii_uppercase)
+    res=res+random.choice(string.digits)
+    res=res+random.choice(string.ascii_lowercase)
+    for i in range(len-3):
+        res = res+random.choice(str1)
+    str_var = list(res)
+    random.shuffle(str_var)
+    print(''.join(str_var))
+
+n = int(input("enter the number"))
+ch(n)
